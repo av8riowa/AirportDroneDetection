8dafde66270f1326bc4cbe97afbd4d0e3521cb1d
av8riowa <160094983+av8riowa@users.noreply.github.com>
2024-06-24 12:32:52 -0500

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,1 +1,13 @@
+# database/models.py
+from app import db
+from datetime import datetime
 
+class DroneData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    drone_id = db.Column(db.String(64), index=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    location = db.Column(db.String(120))
+    telemetry = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f'<DroneData {self.drone_id}>'
